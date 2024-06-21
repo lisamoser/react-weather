52f40f50c3ad3fcf4e522af6576abbfdad8adc53
lisamoser <lisamoser1@hotmail.com>
2024-06-21 18:39:44 +1000

    Began moving project across to VS code

---
+++ src/SearchBar.js
@@ -0,0 +1,59 @@
+import React, { useState } from "react";
+import axios from "axios";
+
+export default function SearchBar() {
+  let [city, setCity] = useState("");
+  let [loaded, setLoaded] = useState(false);
+  let [weather, setWeather] = useState({});
+
+  function displayWeather(response) {
+    setLoaded(true);
+    setWeather({
+      temperature: response.data.main.temp,
+      wind: response.data.wind.speed,
+      humidity: response.data.main.humidity,
+      icon: `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`,
+      description: response.data.weather[0].description,
+    });
+  }
+  function handleSubmit(event) {
+    event.preventDefault();
+
+    let apiKey = "cf6b50b908fa2e0baca3eed8a569a5f6";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(displayWeather);
+  }
+
+  function updateCity(event) {
+    setCity(event.target.value);
+  }
+
+  let form = (
+    <form onSubmit={handleSubmit}>
+      <input type="Search" onChange={updateCity} placeholder="Enter a city.." />
+      <input type="Submit" value="Search" />
+    </form>
+  );
+
+  if (loaded) {
+    return (
+      <div>
+        {form}
+        <h4> The weather for {city} is...</h4>
+        <ul>
+          <li>Temperature: {Math.round(weather.temperature)}°C</li>
+          <li>Description: {weather.description}</li>
+          <li>Humidity: {weather.humidity}%</li>
+          <li>Wind: {weather.wind}km/h</li>
+          <li>
+            {" "}
+            Icon:
+            <img src={weather.icon} alt={weather.description} />
+          </li>
+        </ul>
+      </div>
+    );
+  } else {
+    return form;
+  }
+}
